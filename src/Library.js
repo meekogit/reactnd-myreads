e9831dc3e931fd08d38eee218467cb9ed9b9f367
Maria Caetano <mariag.caetano@yahoo.com>
2018-03-29 19:38:12 -0400

    Create the Library component

---
+++ src/Library.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Bookshelf from './Bookshelf'
+
+function Library(props) {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>{props.title}</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          {props.shelves.map((shelf) =>
+              <Bookshelf
+                title={shelf.title}
+                onChangeShelf={props.onChangeShelf}
+                books={props.books.filter((book) => (book.shelf === shelf.id))}
+              />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Library;
