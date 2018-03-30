9cf2c74ff6b29cc9548c6b7db354cfed952a2722
Maria Caetano <mariag.caetano@yahoo.com>
2018-03-30 15:08:58 -0400

    Search for books by title or author

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import BooksGrid from './BooksGrid';
+import escapeRegExp from 'escape-string-regexp'
 
 class BookSearch extends React.Component {
 
-  state = { query: '' };
-
-  updateQuery = (query) => {
-    this.setState({query: query.replace(/\s+/g, ' ') });
+  state = {
+    query: ''
   };
 
+  updateQuery = (query) => {
+    this.setState({ query: query.replace(/\s+/g, ' ') });
+  };
+
+  filterBooks = (books) => {
+    const match = new RegExp(escapeRegExp(this.state.query), 'i');
+    return books.filter((book) => (
+      match.test(book.title) || book.authors.find((author) => (match.test(author)))
+    ));
+  };
+
+  searchBooks = () => ((this.state.query) ? this.filterBooks(this.props.books) : []);
+
   render() {
+    let searchResults = this.searchBooks();
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -24,7 +38,7 @@ class BookSearch extends React.Component {
         </div>
         <div className="search-books-results">
           <BooksGrid
-            books={this.props.books}
+            books={searchResults}
             onChangeShelf={this.props.onChangeShelf}
           />
         </div>
