95bdd5624b94e7ca067a4acdccd4afc672e06a31
Maria Caetano <mariag.caetano@yahoo.com>
2018-03-30 13:02:48 -0400

    Update state when user inputs a search query

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -2,13 +2,24 @@ import React from 'react';
 import BooksGrid from './BooksGrid';
 
 class BookSearch extends React.Component {
+
+  state = { query: '' };
+
+  updateQuery = (query) => {
+    this.setState({query: query.replace(/\s+/g, ' ') });
+  };
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <a className="close-search">Close</a>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input value={this.state.query}
+              type="text"
+              placeholder="Search by title or author"
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
