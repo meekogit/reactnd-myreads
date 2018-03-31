fd18fed9eb5bec6ea3ac25f0c7ad7f8d5b058426
Maria Caetano <mariag.caetano@yahoo.com>
2018-03-31 19:17:05 -0400

    Improve performance by only updating the modified book

--- src/Book.js
+++ src/Book.js
@@ -1,47 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Book(props) {
+class Book extends React.Component {
 
-  const onChangeShelf = props.onChangeShelf;
-  const book = props.book;
-  const { title, authors, shelf } = props.book;
-  const { thumbnail='' , smallThumbnail='' } = props.book.imageLinks || {};
+  static propTypes = {
+    book: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      authors: PropTypes.arrayOf(PropTypes.string),
+      imageLinks: PropTypes.shape({
+        thumbnail: PropTypes.string,
+        smallThumbnail: PropTypes.string
+      }),
+      shelf: PropTypes.string.isRequired
+    }).isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  };
 
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${thumbnail || smallThumbnail})`}}>
-          {thumbnail || smallThumbnail ? '' : <div className="book-cover-title">No Cover</div>}
+  shouldComponentUpdate = (nextProps, nextState) => (nextProps.book.shelf !== this.props.book.shelf);
+
+  render () {
+    const onChangeShelf = this.props.onChangeShelf;
+    const book = this.props.book;
+    const { title, authors, shelf } = this.props.book;
+    const { thumbnail='' , smallThumbnail='' } = this.props.book.imageLinks || {};
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${thumbnail || smallThumbnail})`}}>
+            {thumbnail || smallThumbnail ? '' : <div className="book-cover-title">No Cover</div>}
+          </div>
+          <div className="book-shelf-changer">
+            <select value={shelf} onChange={(e) => onChangeShelf(book, e.target.value)}>
+              <option disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
-        <div className="book-shelf-changer">
-          <select value={shelf} onChange={(e) => onChangeShelf(book, e.target.value)}>
-            <option disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
-        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{(authors) ? authors.join(', ') : 'No Author'}</div>
       </div>
-      <div className="book-title">{title}</div>
-      <div className="book-authors">{(authors) ? authors.join(', ') : 'No Author'}</div>
-    </div>
-  );
+    );
+  }
 }
-
-Book.propTypes = {
-  book: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    authors: PropTypes.arrayOf(PropTypes.string),
-    imageLinks: PropTypes.shape({
-      thumbnail: PropTypes.string,
-      smallThumbnail: PropTypes.string
-    }),
-    shelf: PropTypes.string.isRequired
-  }).isRequired,
-  onChangeShelf: PropTypes.func.isRequired
-};
 
 export default Book;
